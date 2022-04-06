6538ba334365b048b2c62cb605a79b4e557d9cb9
Sadeesha123 <it20230524@my.sliit.lk>
2022-04-07 01:16:48 +0530

    updated carmanagement front end and back end connection

--- RestAPI/src/app.js
+++ RestAPI/src/app.js
@@ -7,6 +7,12 @@ const CarmanagementRoute = require("./Routers/CarmanagementRoute")
 app.use(express.json())
 app.use(CarmanagementRoute)
 
+app.get('/Carmanagement', (req, res) => {
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+    
+    })
+
+
 app.listen(port,() =>{
     console.log(`connection is setup at Port ${port}`)
 })
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {Route,Routes} from 'react-router-dom';
+import { useEffect } from 'react';
 import './App.css';
 import AboutUs from './components/aboutus.component';
 import Admin from './components/admin.component';
@@ -12,27 +13,43 @@ import Login from './pages/Login';
 import Carmanagement from './components/carmanagement.component';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 import Dashboard from "./pages/Dashboard";
 
 
 function App() {
+  const makeAPICall = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/Carmanagement', {mode:'cors'});
+      const data = await response.json();
+      console.log({ data })
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+  useEffect(() => {
+    makeAPICall();
+  }, [])
+  
   return (
-    <div className="App">
+  <div className="App">
 
 
-     
-<Navbar />
 
-   <Routes>
-    <Route path="/" element={<Homepage/>}/>
-    <Route path="/carlist" element={<CarList/>}/>
-    <Route path="/aboutus" element={<AboutUs/>}/>
-    <Route path="/gallary" element={<Gallary/>}/>
-    <Route path="/admin" element={<Admin/>}/>  
-    <Route path="/Login" element={<Login/>}/>
-    <Route path="/dashboard" element={<Dashboard/>}/> 
-   </Routes>
-    </div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/carlist" element={<CarList />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/gallary" element={<Gallary />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/carmanagement" element={<Carmanagement />} />
+      </Routes>
+          </div>
   );
 }
 

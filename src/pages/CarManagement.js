f1257f0cb9ef2fdff0f1a339ff35988fcf97738a
Sadeesha123 <it20230524@my.sliit.lk>
2022-04-14 13:18:01 +0530

    added post, edit delete car management

--- src/pages/CarManagement.js
+++ src/pages/CarManagement.js
@@ -4,12 +4,38 @@ import { Button, Modal } from "react-bootstrap";
 
 
 const CarManagement = () => {
+    const [Data,setData] = useState([]);
     const [RowData,SetRowData]=useState([])
     const [ViewShow,SetViewShow]=useState(false)
     const handleViewShow = () =>{SetViewShow(true)}
     const handleViewClose = () =>{SetViewShow(false)}
-    const [Data,setData] = useState([]);
-  
+    //for edit model
+    const [ViewEdit,SetEditShow]=useState(false)
+    const handleEditShow = () =>{SetEditShow(true)}
+    const handleEditClose = () =>{SetEditShow(false)}
+
+    //for delete model
+    const [ViewDelete,SetDeleteShow]=useState(false)
+    const handleDeleteShow = () =>{SetDeleteShow(true)}
+    const handleDeleteClose = () =>{SetDeleteShow(false)}
+
+    //for add new data model
+    const [ViewPost,SetPostShow]=useState(false)
+    const handlePostShow = () =>{SetPostShow(true)}
+    const handlePostClose = () =>{SetPostShow(false)}
+
+    //define here that local form that store the data from data
+    const [title,settitle] =useState("")
+    const [email,setemail] =useState("")
+    const [brand,setbrand] =useState("")
+    const [price,setprice] =useState("")
+    const [fuel,setfuel] =useState("")
+    const [model,setmodel] =useState("")
+
+    const [Delete, setDelete] = useState(false)
+    
+    //ID for Update record and Delete
+    const [id,setId] = useState("");
     const GetCarData = () =>{
         //get car data
         const url = 'http://localhost:8000/Carmanagement'
@@ -29,8 +55,70 @@ const CarManagement = () => {
             console.log(err)
         }) 
     }
-    //call this function use in uneffect
 
+    const handleSubmit = () =>{
+        //post car data
+        const url = 'http://localhost:8000/Carmanagement'
+        const Credentials = {title,email,brand,price,fuel,model}
+        axios.post(url, Credentials)
+        .then(response=>{
+            const result = response.data;
+            const {status, message, data}=result;
+            if(status !== 'SUCCESS'){
+                alert(message,status)
+            }
+            else{
+                alert(message)
+                window.location.reload()  
+            }
+        }) 
+        .catch(err=>{
+            console.log(err)
+        })
+    }
+
+    const handleEdit = () => {
+       //Edit car data
+       const url = `http://localhost:8000/Carmanagement/ ${id}`
+       const Credentials = {title,email,brand,price,fuel,model}
+       axios.put(url, Credentials)
+       .then(response=>{
+           const result = response.data;
+           const {status, message, data}=result;
+           if(status !== 'SUCCESS'){
+               alert(message,status)
+           }
+           else{
+               alert(message)
+               window.location.reload()  
+           }
+       }) 
+       .catch(err=>{
+           console.log(err)
+       })
+    }
+
+    //HAndle delete function
+    const handleDelete = () => {
+        //delete car data
+        const url = `http://localhost:8000/Carmanagement/${id}`
+        axios.delete(url)
+        .then(response=>{
+            const result = response.data;
+            const {status, message, data}=result;
+            if(status !== 'SUCCESS'){
+                alert(message,status)
+            }
+            else{
+                alert(message)
+                window.location.reload()  
+            }
+        }) 
+        .catch(err=>{
+            console.log(err)
+        })
+     } 
+    //call this function use in uneffect
     useEffect(()=>{
         GetCarData();
     },[])
@@ -53,9 +141,9 @@ const CarManagement = () => {
 
         <div className='row'>
                 <div className='mt-5 mb-4'>
-                <Button variant='primary'><i className='fa fa-plu'></i>
-                Add New Car
-                </Button> 
+                <Button variant='primary' style={{marginRight:800}} onClick={()=>{handlePostShow()}}><i className='fa fa-plu'></i> Add New Car</Button> 
+                
+                
                     </div>
             </div>
             <div className='row'>
@@ -75,16 +163,16 @@ const CarManagement = () => {
                         <tbody>
                         {Data.map((item)=>
                            <tr key={item._id}>
-                               <td>{item.title}</td>
-                               <td>{item.email}</td>
-                               <td>{item.brand}</td>
-                               <td>{item.price}</td>
-                               <td>{item.fuel}</td>
-                               <td>{item.model}</td>
+                               <td style={{fontSize:25}}>{item.title}</td>
+                               <td style={{fontSize:25}}>{item.email}</td>
+                               <td style={{fontSize:25}}>{item.brand}</td>
+                               <td style={{fontSize:25}}>{item.price}</td>
+                               <td style={{fontSize:25}}>{item.fuel}</td>
+                               <td style={{fontSize:25}}>{item.model}</td>
                                <td style={{minWidth:190}}>
                                    <Button size='sm' variant='primary' onClick={() => {handleViewShow(SetRowData(item))}}>View</Button>
-                                   <Button size='sm' variant='warning'>Edit</Button>
-                                   <Button size='sm' variant='danger'>Delete</Button>
+                                   <Button size='sm' variant='warning' onClick={() => {handleEditShow(SetRowData(item),setId(item._id))}}>Edit</Button>
+                                   <Button size='sm' variant='danger' onClick={() => {handleViewShow(SetRowData(item), setId(item._id), setDelete(true))}}>Delete</Button>
                                </td>
                            </tr>
                            )}
@@ -122,10 +210,103 @@ const CarManagement = () => {
                            <div className='form-group mt-3'>
                                <input type="text" className='form-control' value={RowData.model} readOnly/>
                            </div>
+                           {
+
+                              Delete && (
+                                <Button type='submit' className='btn btn-danger mt-4' onClick={handleDelete}>Delete Car</Button>
+                              )
+
+                           }
                        </div>
                    </Modal.Body>  
                    <Modal.Footer>
                        <Button variant='secondary' onClick={handleViewClose}>Close</Button>
+                   </Modal.Footer>  
+                </Modal>
+
+            </div>
+            {/*model for submit data to database*/}
+            <div className='model-box-view'>
+                <Modal
+                   show={ViewPost}
+                   onHide={handlePostClose}
+                   backdrop="static"
+                   keyboard={false}
+                >
+                   <Modal.Header closeButton>
+                       <Modal.Title>Add a new Car</Modal.Title>
+                   </Modal.Header>
+                   <Modal.Body>
+                       <div>
+                           <div className='form-group'>
+                               <input type="long-text" className='form-control' onChange={(e)=> settitle(e.target.value)} placeholder="Please enter car title"/>
+                           </div>
+                           <div className='form-group mt-3'>
+                               <input type="email" className='form-control' onChange={(e)=> setemail(e.target.value)} placeholder="Please enter an email"/>
+                           </div>
+                           <div className='form-group mt-3'>
+                               <input type="long-text" className='form-control' onChange={(e)=> setbrand(e.target.value)} placeholder="Please enter car brand"/>
+                           </div>
+                           <div className='form-group mt-3'>
+                               <input type="long-text" className='form-control' onChange={(e)=> setprice(e.target.value)} placeholder="Please enter car price"/>
+                           </div>
+                           <div className='form-group mt-3'>
+                               <input type="long-text" className='form-control' onChange={(e)=> setfuel(e.target.value)} placeholder="Please enter fuel type"/>
+                           </div>
+                           <div className='form-group mt-3'>
+                               <input type="long-text" className='form-control' onChange={(e)=> setmodel(e.target.value)} placeholder="Please enter car model"/>
+                           </div>
+                           <Button type='submit' className='btn btn-success mt-4' onClick={handleSubmit}>Add Car</Button>
+                       </div>
+                   </Modal.Body>  
+                   <Modal.Footer>
+                       <Button variant='secondary' onClick={handlePostClose}>Close</Button>
+                   </Modal.Footer>  
+                </Modal>
+
+            </div>
+            {/*Model for edit car record*/}
+            <div className='model-box-view'>
+                <Modal
+                   show={ViewEdit}
+                   onHide={handleEditClose}
+                   backdrop="static"
+                   keyboard={false}
+                >
+                   <Modal.Header closeButton>
+                       <Modal.Title>Edit Car</Modal.Title>
+                   </Modal.Header>
+                   <Modal.Body>
+                       <div>
+                           <div className='form-group'>
+                               <label>Title</label>
+                               <input type="long-text" className='form-control' onChange={(e)=> settitle(e.target.value)} placeholder="Please enter car title" defaultValue={RowData.title}/>
+                           </div>
+                           <div className='form-group mt-3'>
+                           <label>Email</label>
+                               <input type="email" className='form-control' onChange={(e)=> setemail(e.target.value)} placeholder="Please enter an email" defaultValue={RowData.email}/>
+                           </div>
+                           <div className='form-group mt-3'>
+                           <label>Brand</label>
+                               <input type="long-text" className='form-control' onChange={(e)=> setbrand(e.target.value)} placeholder="Please enter car brand" defaultValue={RowData.brand}/>
+                           </div>
+                           <div className='form-group mt-3'>
+                           <label>Price</label>
+                               <input type="long-text" className='form-control' onChange={(e)=> setprice(e.target.value)} placeholder="Please enter car price" defaultValue={RowData.price}/>
+                           </div>
+                           <div className='form-group mt-3'>
+                           <label>Fuel</label>
+                               <input type="long-text" className='form-control' onChange={(e)=> setfuel(e.target.value)} placeholder="Please enter fuel type" defaultValue={RowData.fuel}/>
+                           </div>
+                           <div className='form-group mt-3'>
+                           <label>Model</label>
+                               <input type="long-text" className='form-control' onChange={(e)=> setmodel(e.target.value)} placeholder="Please enter car model" defaultValue={RowData.model}/>
+                           </div>
+                           <Button type='submit' className='btn btn-warning mt-4' onClick={handleEdit}>Edit Car</Button>
+                       </div>
+                   </Modal.Body>  
+                   <Modal.Footer>
+                       <Button variant='secondary' onClick={handleEditClose}>Close</Button>
                    </Modal.Footer>  
                 </Modal>
 
